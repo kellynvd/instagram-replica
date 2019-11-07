220793dc9a68076fbdbf8d6bcac6526c2a8b5cc7
kellynvd <kellynvd@gmail.com>
2019-11-07 15:58:31 -0300

    Add show and index

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,5 +1,11 @@
 class PicsController < ApplicationController
+  before_action :find_pic, only: %i[show edit update destroy]
+
   def index
+    @pics = Pic.all.order('created_at DESC')
+  end
+
+  def show
   end
 
   def new
@@ -8,9 +14,19 @@ class PicsController < ApplicationController
 
   def create
     @pic = Pic.new(pic_params)
+
+    if @pic.save
+      redirect_to @pic, notice: 'Yesss! It was posted!'
+    else
+      render 'new'
+    end
   end
 
   private
+
+  def find_pic
+    @pic = Pic.find(params[:id])
+  end
 
   def pic_params
     params.require(:pic).permit(:title, :description)
